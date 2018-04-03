5530ccd455dab060988d1b18e533a2a93fe0424b
David Meister <thedavidmeister@gmail.com>
2018-04-03 23:20:49 +1000

    spew in parallel

--- src/tailrecursion/boot_bucket.clj
+++ src/tailrecursion/boot_bucket.clj
@@ -35,9 +35,12 @@
          tgt-digests (pod/with-call-in pod
                        (tailrecursion.boot-bucket.client/list-digests ~*opts*))
          src-files   (remove #(some (fn [[p h]] (and (= (:path %) p) (= (:hash %) h))) tgt-digests) src-files*)]
-    (doseq [{:keys [dir path]} src-files]
-     (util/info "• %s\n" path)
-     (pod/with-call-in pod
-      (tailrecursion.boot-bucket.client/put-file! ~*opts* ~(.getPath dir) ~path))
-     (boot/add-meta fileset (into {} (mapv #(vector (:path %) {::uploaded true}) src-files))))
+    (dorun
+     (pmap
+      (fn [{:keys [dir path]}]
+       (util/info "• %s\n" path)
+       (pod/with-call-in pod
+        (tailrecursion.boot-bucket.client/put-file! ~*opts* ~(.getPath dir) ~path))
+       (boot/add-meta fileset (into {} (mapv #(vector (:path %) {::uploaded true}) src-files))))
+      src-files))
     fileset))))
